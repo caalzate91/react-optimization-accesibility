1d06c70c47cf022a0f67674cfa312dbef9b490c0
William Castro <wmcastro@bancolombia.com.co>
2025-10-02 00:00:25 -0500

    fix: correct moduleNameMapping to moduleNameMapper in jest configuration

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  moduleNameMapping: {
+  moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/__mocks__/fileMock.js'
   },
